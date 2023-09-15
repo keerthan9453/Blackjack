9fc9c687cad8fd0b41f6c5ad898a8480c1af9760
Keerthan <69869523+keerthan9453@users.noreply.github.com>
2023-09-15 14:51:25 -0400

    Update blackjack.js

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 
-let firstCard = 10
-let SecondCard = 4
-let cards = [firstCard, SecondCard]
-let sum = firstCard + SecondCard
+
+let cards = []
+let sum = 0
 let message = ""
-let isAlive = true
+let isAlive = false
 let hasBlackJack = false
 let sumel = document.getElementById("sum-el")
 let MessageEl = document.getElementById("message-el")
 
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Per",
+    chip: 145
+}
 
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $"+ player.chip
+function randomNumber()
+{
+    let random_num = Math.floor( Math.random() * 13) + 1
+    
+    if (random_num > 10)
+    {
+        return 10
+    }
+    else if (random_num == 1){
+        return 11
+    }
+    else {
+        return random_num
+    }
+}
 function StartGame(){
+    isAlive = true
+    let firstCard = randomNumber()
+    let SecondCard = randomNumber() 
+    cards = [firstCard , SecondCard]
+    sum = firstCard + SecondCard
     renderGame()
 }
 function renderGame(){
@@ -35,9 +61,13 @@ function renderGame(){
     MessageEl.textContent = message
 }
 function NewCards() {
-    console.log("Drawing a new card from the deck")
-    let card = 8
-    sum+= card
-    cards.push(card)
-    renderGame()
+    //console.log("Drawing a new card from the deck")
+    if (isAlive == true && hasBlackJack == false){
+        let card = 8
+        sum+= card
+        cards.push(card)
+        renderGame()
+        
+    }
+    
 }
